f4fa5810c949a0ad92679fe39047b1adf90ac815
Navojith <navojith22@gmail.com>
2023-04-12 16:06:44 +0530

    card

--- dev/react/foss-landing/submissions/pawara-navojith/src/components/Landing/ClubInfo/ClubInfo.jsx
+++ dev/react/foss-landing/submissions/pawara-navojith/src/components/Landing/ClubInfo/ClubInfo.jsx
@@ -7,7 +7,7 @@ import Col from 'react-bootstrap/esm/Col';
 const ClubInfo = () => {
   return (
     <div>
-      <div className="grid grid-cols-12 px-[32px] pb-[32px] min-[768px]:px-12 min-[768px]:pb-12 min-[992px]:px-20 min-[992px]:pb-20 ">
+      <div className="grid mt-[2rem] grid-cols-12 px-[32px] pb-[32px] min-[768px]:px-12 min-[768px]:pb-12 min-[992px]:px-20 min-[992px]:pb-20 ">
         <div className="px-[15px] max-[575px]:col-span-12 min-[576px]:col-span-6 min-[768px]:col-span-3">
           <Card icon={'fa-calendar'} number={7} title={'Events'} />
         </div>
